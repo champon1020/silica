7e541f67aeda685b37653000432268737fc1c97d
champon <nagatelu1020@gmail.com>
2019-04-24 22:23:12 +0900

    creating shell sort algorithm now

---
+++ src/3_6_ShellSortalfheim/.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+void insertionSort(int array[], int n, int g){
+    for(int i=g; i<n; i++){
+        int value = array[i];
+    }
+}
+
+int main(){
+    int n;
+    int array[1000000];
+
+    cout << "num: ";
+    cin >> n;
+    cout << "array:" << endl;
+    for(int i=n; i<n; i++){
+        cin >> array[i];
+    }
+
+}
