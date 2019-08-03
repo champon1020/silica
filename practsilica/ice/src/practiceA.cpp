fbecd5476f5a2fef46d6090bfe53576ab45dea97
champon <nagatelu1020@gmail.com>
2019-08-03 14:27:43 +0900

    first commit

---
+++ practsilica/ice/src/practiceA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int a, b;
+    int c;
+    string s;
+    cin >> a >> b;
+    cin >> c;
+    cin >> s;
+
+    cout << a+b+c << " " << s << endl;
+    return 0;
+}
