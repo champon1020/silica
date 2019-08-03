258781d091113c27038cc9120331860ba81ff084
champon1020 <nagatelu1020@gmail.com>
2019-08-03 21:09:35 +0900

    #20190803

--- practsilica/ice/src/ABC083B.cpp
+++ practsilica/ice/src/ABC083B.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 
+int calcSum(int num){
+    int sum = 0;
+    while(num){
+        int dig = num%10;
+        sum += dig;
+        num /= 10;
+    }
+    return sum;
+}
+
+
+int main(){
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int sum = 0;
+    for(int i=1; i<=n; i++){
+        int p = calcSum(i);
+        if(a <= p && p <= b){
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
