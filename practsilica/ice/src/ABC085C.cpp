499b1afd5e7de896af5051fd8329bf5a531bcdf0
champon1020 <nagatelu1020@gmail.com>
2019-08-05 21:18:02 +0900

    ABC085B

---
+++ practsilica/ice/src/ABC085C.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int checkFromTop(int n, int y){
+    for(int i=n; i>=0; i++){
+        if(i*10000 < y){
+            return 0;
+        }else if(i*10000 > y){
+            
+        }
+
+    }
+}
+
+
+int main(){
+    int n, y;
+    cin >> n >> y;
+    bool result = false;
+
+    while(true){
+    }
+}
