bd473a1aad3d535eabbfe5d1bda2287a5d7d24e9
champon1020 <nagatelu1020@gmail.com>
2019-09-15 20:43:25 +0900

    abc140e abc140f

--- beginer/abc140/abc140e.cpp
+++ beginer/abc140/abc140e.cpp
@@ -1,44 +1,20 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
+using Int = long long int;
+template<typename T> void swap(T *t1, T *t2){ T* tmp=t1; t1=t2; t2=tmp;}
 
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, p[100000];
+    Int n, p[100000];
+    vector<Int> map;
     cin >> n;
-    for(int i=0; i<n; i++){
+    for(Int i=0; i<n; i++){
         cin >> p[i];
     }
-    int max = 0;
-    int max2 = 0;
-    int sum = 0;
-    for(int i=0; i<n-1; i++){
-        int max = 0;
-        int max2 = 0;
-        for(int j=i+1; j<n; j++){
-            if(i+1==j) {
-                sum += p[i] > p[j] ? p[j] : p[i];
-                max = p[i] < p[j] ? p[j] : p[i];
-                max2 = sum;
-                continue;
-            }
-            if(p[j] > max) {
-                sum += max;
-                max2 = max;
-                max = p[j];
-            }else if(p[j] < max && p[j] > max2) {
-                sum += p[j];
-                max2 = p[j];
-            }else {
-                sum += max2;
-            }
-        }
+    for(Int i=0; i<n; i++){
+        
     }
     cout << sum << endl;
     return 0;
