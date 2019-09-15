e04ab0b3933b9e982f2aba16471402ad1e4dbc0d
champon1020 <nagatelu1020@gmail.com>
2019-09-15 23:08:36 +0900

    abc138c

--- beginer/abc138/abc138c.cpp
+++ beginer/abc138/abc138c.cpp
@@ -8,7 +8,22 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    
-
+    double n;
+    vector<double> v;
+    double tmp;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    while(v.size() > 1){
+        sort(v.begin(), v.end());
+        double a, b;
+        a = v[0];
+        b = v[1];
+        v.erase(v.begin(), v.begin()+2);
+        v.push_back((a+b)/2);
+    }
+    printf("%.6lf\n", v[0]);
     return 0;
 }
