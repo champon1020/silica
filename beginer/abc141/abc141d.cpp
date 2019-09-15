56aaa844c7db625730aac9086f6d5f92d2c6f40f
champon1020 <nagatelu1020@gmail.com>
2019-09-16 00:26:30 +0900

    abc141d

--- beginer/abc141/abc141d.cpp
+++ beginer/abc141/abc141d.cpp
@@ -7,30 +7,23 @@ template<typename T> void swap(T *t1, T *t2){ T* tmp=t1; t1=t2; t2=tmp;}
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    Int n, m;
+
+    Int n, m, tmp;
+    priority_queue<int> q;
     cin >> n >> m;
-    vector<Int> a;
-    Int tmp;
-    for(int i=0; i<n; i++){
+    for(Int i=0; i<n; i++){
         cin >> tmp;
-        a.push_back(tmp);
+        q.push(tmp);
     }
-    sort(a.begin(), a.end());
-    Int p = 0;
+    for(Int i=0; i<m; i++){
+        Int buf = q.top();
+        q.pop();
+        q.push(buf/2);
+    }
     Int sum = 0;
-    for(int i=n-1; i>=0; i--){
-        cout << m << endl;
-        if(m > 1){
-            for(int j=0; j<=m; j++){
-                if(floor(a[i] / pow(2, j)) <= 1){
-                    p = j-1;
-                    break;
-                }
-            }
-        }else{
-            p = (m == 1) ? 1 : 0;
-        }
-        m -= p;
+    for(Int i=0; i<n; i++){
+        sum += q.top();
+        q.pop();
     }
     cout << sum << endl;
     return 0;
