b554a63085cbfb0f76c2dbaa56da2234a5a12317
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-09-22 16:05:03 +0900

    abc128a-c

---
+++ beginer/abc128/abc128d.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Int = long long int;
+template<typename T> void swap(T *t1, T *t2){ T* tmp=t1; t1=t2; t2=tmp;}
+Int tmpi = 0;
+double tmpd = 0.0;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+}
