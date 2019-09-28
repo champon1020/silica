bd11a49b08a8380e1299563c34b7418e955e8c3e
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-09-28 14:21:53 +0900

    abc125c

--- beginer/abc125/abc125c.cpp
+++ beginer/abc125/abc125c.cpp
@@ -1,57 +1,63 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 using Int = long long int;
 template<typename T> void swap(T *t1, T *t2){ T* tmp=t1; t1=t2; t2=tmp;}
-Int tmpi = 0;
-double tmpd = 0.0;
 
+
+Int gcd(Int a, Int b){
+    Int p, q, r, s;
+    p = a > b ? a : b;
+    q = a > b ? b : a;
+    while(true){
+        //cout << p << " " << q << " " << s << " " << r << endl;
+        s = p/q;
+        r = p%q;
+        if(r==0){
+            break;
+        }else{
+            p = q;
+            q = r;
+        }
+    }
+    return q;
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    Int n;
+    Int n, tmpi;
     vector<Int> a;
     cin >> n;
-    for(Int i=0; i<n; i++){
+    for(int i=0; i<n; i++){
         cin >> tmpi;
         a.push_back(tmpi);
     }
 
-    sort(a.begin(), a.end());
-    Int p=a[n-1], q=a[n-2];
-    Int r=-1, s=-1;
-    Int g=0;
-    Int gbuf = 0;
-    tmpi = -1;
-    for(Int i=n-2; i>=0; i--){
-        if(i!=n-2){
-            p = a[i];
-            q = g;
+    Int l[100001], r[100001];
+    l[0] = a[0];
+    r[n-1] = a[n-1];
+
+    for(int i=1; i<n-1; i++){
+        l[i] = gcd(l[i-1], a[i]);
+    }
+    for(Int i=n-2; i>=1; i--){
+        r[i] = gcd(r[i+1], a[i]);
+    }
+    Int max = 0;
+    for(int i=0; i<n; i++){
+        Int m = 0;
+        if(i==0){
+            m = r[1];
+        }else if(i==n-1){
+            m = l[n-2];
+        }else{
+            m = gcd(l[i-1], r[i+1]);
         }
-        while(true){
-            s = p/q;
-            r = p%q;
-            if(r==0){
-                gbuf = g;
-                g = q;
-                break;
-            }else{
-                p = q;
-                q = r;
-            }
-        }
-        if(g == 1){
-            if(tmpi!=-1){
-                cout << "1" << endl;
-                return 0;
-            }else{
-                tmpi = i;
-            }
-        }
+        max = m > max ? m : max;
     }
 
-    cout << gbuf << endl;
-
+    cout << max << endl;
     return 0;
 }
