3c1758b21fa14497e227f0b459c6aaebcc22c6f8
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-03 22:01:35 +0900

    update lib

--- beginer/abc124/abc124d.cpp
+++ beginer/abc124/abc124d.cpp
@@ -10,40 +10,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    Int n, k;
-    char s[1000001];
-    cin >> n >> k;
-    cin >> s;
-
-    vector<Int> v;
-    Int i=0;
-    Int cnt=0;
-    char marker = s[0];
-    while(i<n){
-        if(s[i]==marker){
-            cnt++;
-        }else{
-            v.push_back(cnt);
-            marker=s[i];
-            cnt=1;
-        }
-        i++;
-    }
-    if(cnt!=0){
-        v.push_back(cnt);
-    }
-
-    Int max=0;
-    if(s[0]=='1'){
-        for(Int i=0; i<n-2*k-1; i+=2){
-            Int sum=0;
-            for(Int j=i; j<i+2*k+1; j++){
-                sum += v[j];
-            }
-            max = sum > max ? sum : max;
-        }
-    }
-    cout << max << endl;
+    
 
     return 0;
 }
