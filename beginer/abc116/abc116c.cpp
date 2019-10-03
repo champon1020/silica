a188663cb05bcd99750f68b8674f77fadcde930a
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-04 00:57:57 +0900

    abc115c abc116c

---
+++ beginer/abc116/abc116c.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; i++)
+#define repr(i, n) for(int i=n; i>=0; i--)
+#define reps(i, s, e) for(int i=s; i<e; i++)
+#define inf 1e10
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<lli>())
+#define sz(x) x.size()
+#define ceil(a, b) (a+b-1)/b
+#define ok cout << "ok" << endl;
+using namespace std;
+using lli = long long int;
+template<typename T> bool chmax(T &a, T &b){ if(a<b) { a=b; return 1; }}
+template<typename T> bool chmin(T &a, T &b){ if(b<a) { a=b; return 1; }}
+template<typename T> T gcd(T a, T b){ if(b==0) return a; return gcd(b, a%b); }
+template<typename T> T lcm(T a, T b){ return a*b/gcd(a, b); }
+template<typename T> T vdebug(vector<T> v){ for(auto vv : v){ cout << vv << " "; } cout << endl; }
+template<typename T> T arrdebug(T arr[], int n){ rep(i, n){ cout << arr[i] << " "; } cout << endl; }
+void ans(bool b){ if(b) cout << "Yes" << endl; else cout << "No" << endl; }
+void ans2(bool b){ if(b) cout << "YES" << endl; else cout << "NO" << endl; }
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    lli n;
+    lli h[500];
+
+    cin >> n;
+    rep(i, n){
+        cin >> h[i];
+    }
+
+    int ans = 0;
+
+    while(true){
+        int cnt=0;
+        bool flg=true;
+        rep(i, n){
+            if(h[i]!=0){
+                h[i]--;
+                cnt=1;
+                flg=false;
+            }else{
+                ans+=cnt;
+                cnt=0;
+            }
+        }
+        ans += cnt;
+        if(flg) break;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
