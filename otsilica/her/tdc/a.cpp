bb619b1783d464a75ddad1be4e7a60224b9e6bb7
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-07 19:15:54 +0900

    tdc not complete

---
+++ otsilica/her/tdc/a.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; i++)
+#define repr(i, n) for(int i=n; i>=0; i--)
+#define reps(i, s, e) for(int i=s; i<e; i++)
+#define inf 1e18
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<lli>())
+#define sz(x) x.size()
+#define ceil(a, b) (a+b-1)/b
+#define ok cout << "ok" << endl;
+using namespace std;
+using ll = long long;
+template<typename T> T gcd(T a, T b){ if(b==0) return a; return gcd(b, a%b); }
+template<typename T> T lcm(T a, T b){ return a*b/gcd(a, b); }
+template<typename T> T vdebug(vector<T> v){ for(auto vv : v){ cout << vv << " "; } cout << endl; }
+template<typename T> T adebug(T arr[], int n){ rep(i, n){ cout << arr[i] << " "; } cout << endl; }
+void ans(bool b){ if(b) cout << "Yes" << endl; else cout << "No" << endl; }
+void ans2(bool b){ if(b) cout << "YES" << endl; else cout << "NO" << endl; }
+int keta(ll num){ int k=0; while(num>0){ num/=10; k++; } return k; }
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+    
+    
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, tmpint;
+    vector<int> p;
+    cin >> n;
+    rep(i, n){
+        cin >> tmpint;
+        p.push_back(tmpint);
+    }
+    
+    int dp[100001];
+    rep(i, 10000){
+        dp[i] = 0;
+    }
+    dp[0] = 1;
+
+    rep(i, n){
+        vector<int> buf;
+        rep(j, 10000){
+            if(dp[j] == 1){
+                buf.push_back(p[i] + j);
+            }
+        }
+        for(auto b : buf){
+            dp[b] = 1;
+        }
+    }
+
+    int res = 0;
+    rep(i, 10000){
+        if(dp[i]) res++;
+    }
+
+    cout << res << endl;
+    
+    return 0;
+}
