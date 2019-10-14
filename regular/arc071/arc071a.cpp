8d5e20ac4b070eaf49f425f7d78eb7b018e7eb39
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-14 20:49:46 +0900

    some

---
+++ regular/arc071/arc071a.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; i++)
+#define repr(i, s, e) for(int i=s; i>=e; i--)
+#define reps(i, s, e) for(int i=s; i<e; i++)
+#define inf 1e18
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<ll>())
+#define sz(x) x.size()
+#define ceil(a, b) (a+b-1)/b
+#define ok cout << "ok" << endl;
+using namespace std;
+using ll = long long;
+template<typename T> inline bool chmax(T &a, T b){ if(a<b){ a=b; return true; } return false; }
+template<typename T> inline bool chmin(T &a, T b){ if(b<a){ a=b; return true; } return false; }
+template<typename T> T gcd(T a, T b){ if(b==0) return a; return gcd(b, a%b); }
+template<typename T> T lcm(T a, T b){ return a*b/gcd(a, b); }
+template<typename T> void vdebug(vector<T> v){ for(auto vv : v){ cout << vv << " "; } cout << endl; }
+template<typename T> void adebug(T arr[], int n){ rep(i, n){ cout << arr[i] << " "; } cout << endl; }
+void ans(bool b){ if(b) cout << "Yes" << endl; else cout << "No" << endl; }
+void ans2(bool b){ if(b) cout << "YES" << endl; else cout << "NO" << endl; }
+int keta(ll num){ int k=0; while(num>0){ num/=10; k++; } return k; }
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int alphabet[26][100];
+    int n;
+    string s;
+
+    rep(i, 26){
+        rep(j, 100){
+            alphabet[i][j] = 0;
+        }
+    }
+
+    cin >> n;
+    rep(i, n){
+        cin >> s;
+        rep(j, s.length()){
+            alphabet[s[j]-'a'][i]++;
+        }
+    }
+
+    rep(i, 26){
+        int mini = 10000;
+        rep(j, n){
+            mini = min(mini, alphabet[i][j]);
+            if(mini==0) break;
+        }
+
+        rep(k, mini){
+            cout << char(i+'a');
+        }
+        if(mini == 0){
+            cout << "";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
