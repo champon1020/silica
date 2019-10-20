b59bcfa21697f4a225fb26089f2dcdeabefb8d80
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-21 02:07:45 +0900

    arc098b now

---
+++ regular/arc098/arc098b.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; i++)
+#define repr(i, s, e) for(int i=s; i>=e; i--)
+#define reps(i, s, e) for(int i=s; i<e; i++)
+#define inf 1e18
+#define all(v) v.begin(),v.end()
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<ll>())
+#define sz(x) x.size()
+#define ceil(a, b) (a+b-1)/b
+#define ok cout << "ok" << endl;
+using namespace std;
+using ll = long long;
+template<typename T> inline bool chmax(T &a, T b){ if(a<b){ a=b; return true; } return false; }
+template<typename T> inline bool chmin(T &a, T b){ if(b<a){ a=b; return true; } return false; }
+template<typename T> T gcd(T a, T b){ if(b==0) return a; return gcd(b, a%b); }
+template<typename T> T lcm(T a, T b){ return a*b/gcd(a, b); }
+template<typename T> void vdebug(vector<T> v){ for(auto vv : v){ cout << vv << " "; } cout << endl; }
+template<typename T> void adebug(T arr[], int n){ rep(i, n){ cout << arr[i] << " "; } cout << endl; }
+void ans(bool b){ if(b) cout << "Yes" << endl; else cout << "No" << endl; }
+void ans2(bool b){ if(b) cout << "YES" << endl; else cout << "NO" << endl; }
+int keta(ll num){ int k=0; while(num>0){ num/=10; k++; } return k; }
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+const int MAX = 100010;
+
+class Bit {
+    ll bit[MAX+1];
+    int n;
+
+public:
+    Bit(int nn){
+        n = nn;
+    }
+
+    void add(int i, ll x) {
+        while(i <= n){
+            bit[i] += x;
+            i += i&-i;
+        }
+    }
+
+    ll sum(int i){
+        ll s = 0;
+        while(i > 0){
+            s += bit[i];
+            i -= i&-i;
+        }
+        return s;
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, tmp;
+    vector<ll> a;
+    cin >> n;
+    rep(i, n){
+        cin >> tmp;
+        a.push_back(tmp);
+    }
+
+    Bit tree(n);
+    rep(i, n){
+        tree.add(i+1, a[i]);
+    }
+
+    int l=0, r=0;
+    while(l <= n && r <= n){
+        
+    }
+
+    return 0;
+}
