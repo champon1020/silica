da1de99bf2711a751c5b55d5819555e614a52d2e
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-30 20:27:01 +0900

    fix

---
+++ lib/graph/Dijkstsilica/ra.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; i++)
+#define repr(i, n) for(int i=n; i>=0; i--)
+#define reps(i, s, e) for(int i=s; i<e; i++)
+#define inf 1e18
+#define ll long long
+using namespace std;
+
+
+class Dijkstra{
+  public:
+    
+};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+
+    return 0;
+}
