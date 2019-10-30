5959c1c13f6a6386096435250f19a8adb3955ef3
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-10-31 00:48:13 +0900

    some

---
+++ grand/agc034/agc034a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(ll i=0; i<n; i++)
+#define repr(i, s, e) for(ll i=s; i>=e; i--)
+#define reps(i, s, e) for(ll i=s; i<=e; i++)
+#define ll long long
+#define inf 1e18
+#define all(v) v.begin(),v.end()
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<ll>())
+#define sz(x) x.size()
+#define ceil(a, b) (a+b-1)/b
+#define ok cout << "ok" << endl;
+#define sp << " " <<
+using namespace std;
+template<typename T> inline bool chmax(T &a, T b){ if(a<b){ a=b; return true; } return false; }
+template<typename T> inline bool chmin(T &a, T b){ if(b<a){ a=b; return true; } return false; }
+template<typename T> T gcd(T a, T b){ if(b==0) return a; return gcd(b, a%b); }
+template<typename T> T lcm(T a, T b){ return a*(b/gcd(a, b)); }
+template<typename T> void vdebug(vector<T> v){ for(auto vv : v){ cout << vv << " "; } cout << endl; }
+template<typename T> void adebug(T arr[], ll n){ rep(i, n){ cout << arr[i] << " "; } cout << endl; }
+void ans(bool b){ if(b) cout << "Yes" << endl; else cout << "No" << endl; }
+void ans2(bool b){ if(b) cout << "YES" << endl; else cout << "NO" << endl; }
+ll keta(ll num){ ll k=0; while(num>0){ num/=10; k++; } return k; }
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s, t = "";
+    cin >> s;
+
+    int i = 0;
+    while(i < s.length()){
+        //cout << i << endl;
+        if(s[i] == 'A'){
+            t += s[i];
+        }else if(i > 0 && s[i] == 'B' && s[i+1] == 'C'){
+            t += 'X';
+            i++;
+        }else{
+            t += s[i];
+        }
+        i++;
+    }
+
+    ll cnt = 0;
+    ll res = 0;
+    rep(i, t.length()){
+        if(t[i] == 'A'){
+            cnt++;
+            continue;
+        }
+        if(t[i] == 'X') res += cnt;
+        else cnt = 0;
+    }
+
+    //cout << t << endl;
+    cout << res << endl;
+
+    return 0;
+}
