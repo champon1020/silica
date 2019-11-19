6359942bcc9d749b5396e7122056d5145877ec74
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-11-20 01:22:16 +0900

    solved

--- beginer/abc051/abc051c.cpp
+++ beginer/abc051/abc051c.cpp
@@ -1,1 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, n) for(ll i=0; i<n; i++)
+#define repr(i, s, e) for(ll i=s; i>=e; i--)
+#define reps(i, s, e) for(ll i=s; i<=e; i++)
+#define inf 1e18
+#define all(v) v.begin(),v.end()
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<ll>())
+#define ceil(a, b) (a+b-1)/b
+#define ok cout << "ok" << endl;
+#define sp << " " <<
+template<typename T> inline bool chmax(T &a, T b){ if(a<b) a=b; return a<b; }
+template<typename T> inline bool chmin(T &a, T b){ if(b<a) a=b; return b<a; }
+template<typename T> void vdeb(T v){
+    cout << "#vdebug" << endl;
+    for(auto vv : v) cout << vv << " ";
+    cout << endl;
+}
+template<typename T1, typename T2> void adeb(T1 arr[], T2 n){
+    cout << "#adebug" << endl;
+    for(int i=0; i<=n; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+template<typename T1> void mdeb(T1 mp){
+    cout << "#map pair debug" << endl;
+    for(auto const& m : mp) cout << m.first sp m.second << endl;
+}
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){ fill((T*)array, (T*)(array+N), val); }
+void ans(bool b){ cout << (b ? "Yes" : "No") << endl; }
+void ans2(bool b){ cout << (b ? "YES" : "NO") << endl; }
+int dx[] = {1, 0, -1, 0, 1, -1, 1, -1};
+int dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
 
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int sx, sy, tx, ty;
+    cin >> sx >> sy >> tx >> ty;
+
+    int diffx = abs(sx - tx);
+    int diffy = abs(sy - ty);
+
+    string res = "";
+    rep(i, diffx){
+        res += 'R';
+    }
+    rep(i, diffy){
+        res += 'U';
+    }
+
+    rep(i, diffx){
+        res += 'L';
+    }
+    rep(i, diffy){
+        res += 'D';
+    }
+
+    res += 'D';
+    rep(i, diffx+1){
+        res += 'R';
+    }
+    rep(i, diffy+1){
+        res += 'U';
+    }
+    res += 'L';
+
+    res += 'U';
+    rep(i, diffx+1){
+        res += 'L';
+    }
+    rep(i, diffy+1){
+        res += 'D';
+    }
+    res += 'R';
+
+    cout << res << endl;
+
+    return 0;
+}
