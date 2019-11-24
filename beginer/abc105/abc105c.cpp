a20575b892a28b490e6862e2f13223910209174f
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2019-11-25 01:00:11 +0900

    solve

--- beginer/abc105/abc105c.cpp
+++ beginer/abc105/abc105c.cpp
@@ -1,24 +1,36 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for(int i=0; i<n; i++)
-#define repr(i, n) for(int i=n; i>=0; i--)
-#define reps(i, s, e) for(int i=s; i<e; i++)
+using namespace std;
+#define ll long long
+#define rep(i, n) for(ll i=0; i<n; i++)
+#define repr(i, s, e) for(ll i=s; i>=e; i--)
+#define reps(i, s, e) for(ll i=s; i<=e; i++)
 #define inf 1e18
+#define all(v) v.begin(),v.end()
 #define vsort(v) sort(v.begin(), v.end())
 #define vsortr(v) sort(v.begin(), v.end(), greater<ll>())
-#define sz(x) x.size()
 #define ceil(a, b) (a+b-1)/b
 #define ok cout << "ok" << endl;
-using namespace std;
-using ll = long long;
-template<typename T> inline bool chmax(T &a, T b){ if(a<b){ a=b; return true; } return false; }
-template<typename T> inline bool chmin(T &a, T b){ if(b<a){ a=b; return true; } return false; }
-template<typename T> T gcd(T a, T b){ if(b==0) return a; return gcd(b, a%b); }
-template<typename T> T lcm(T a, T b){ return a*b/gcd(a, b); }
-template<typename T> T vdebug(vector<T> v){ for(auto vv : v){ cout << vv << " "; } cout << endl; }
-template<typename T> T adebug(T arr[], int n){ rep(i, n){ cout << arr[i] << " "; } cout << endl; }
-void ans(bool b){ if(b) cout << "Yes" << endl; else cout << "No" << endl; }
-void ans2(bool b){ if(b) cout << "YES" << endl; else cout << "NO" << endl; }
-int keta(ll num){ int k=0; while(num>0){ num/=10; k++; } return k; }
+#define sp << " " <<
+template<typename T> inline bool chmax(T &a, T b){ if(a<b) a=b; return a<b; }
+template<typename T> inline bool chmin(T &a, T b){ if(b<a) a=b; return b<a; }
+template<typename T> void vdeb(vector<T> v){
+    cout << "#vdebug" << endl;
+    for(auto vv : v) cout << vv << " ";
+    cout << endl;
+}
+template<typename T1, typename T2> void adeb(T1 arr[], T2 n){
+    cout << "#adebug" << endl;
+    for(int i=0; i<=n; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+template<typename T1> void mdeb(T1 mp){
+    cout << "#map pair debug" << endl;
+    for(auto const& m : mp) cout << m.first sp m.second << endl;
+}
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){ fill((T*)array, (T*)(array+N), val); }
+void ans(bool b){ cout << (b ? "Yes" : "No") << endl; }
+void ans2(bool b){ cout << (b ? "YES" : "NO") << endl; }
 int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
 int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
 
@@ -27,7 +39,29 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    
+    ll n;
+    cin >> n;
+
+    int k = 0;
+    int bufn = abs(n);
+    while(pow(2, k) <= bufn) k++;
+
+    rep(i, k){
+        if(bufn & 1<<i) bufn &= ~(1<<i);
+        else bufn |= (1<<i);  
+    }
+    cout << bufn << endl;
+
+    vector<int> binary;
+    repr(i, k-1, 0){
+        if(bufn & 1<<i) binary.push_back(1);
+        else binary.push_back(0);
+    }
+
+    for(auto const& e : binary){
+        cout << e;
+    }
+    cout << endl;
 
     return 0;
 }
