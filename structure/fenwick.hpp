801aa3318a5d9b2f5d15423ff561d5791636826d
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-04-27 22:44:56 +0900

    update lib

--- structure/fenwick.hpp
+++ structure/fenwick.hpp
@@ -4,23 +4,23 @@
 #include <vector>
 using namespace std;
 
-namespace bit {
+namespace fenwick {
 
     using ll = long long;
 
-    class Bit {
+    class Fenwick {
     public:
         ll n;
         vector<ll> bit;
 
-        Bit(ll nn, ll e);
+		Fenwick(ll nn, ll e);
         ll sum(ll i);
         void add(ll i, ll x);
     };
 
-    Bit::Bit(ll nn, ll e):n(nn), bit(nn + 1, e) {}
+    Fenwick::Fenwick(ll nn, ll e):n(nn), bit(nn + 1, e) {}
 
-    ll Bit::sum(ll i) {
+    ll Fenwick::sum(ll i) {
         ll s = 0LL;
         while (i > 0) {
             s += bit[i];
@@ -29,7 +29,7 @@ namespace bit {
         return s;
     }
 
-    void Bit::add(ll i, ll x) {
+    void Fenwick::add(ll i, ll x) {
         while (i <= n) {
             bit[i] += x;
             i += i & -i;
--- structure/segtree.hpp
+++ structure/segtree.hpp
@@ -4,17 +4,21 @@
 #include <vector>
 using namespace std;
 
-namespace seg {
+namespace segtree {
 
     using ll = long long;
     const ll inf = 1e18;
 
-    class SegmentTree {
+    class RMaxQ {
     public:
         ll n;
         vector<ll> dat;
 
-        SegmentTree(int nn);
+        explicit RMaxQ(int nn) {
+			n = 1;
+			while (n < nn) n *= 2;
+			dat.resize(2 * n - 1, -inf);
+		}
 
         void update(ll k, ll a);
         ll query(ll a, ll b, ll k, ll l, ll r);
@@ -22,38 +26,78 @@ namespace seg {
         ll operator[](int i);
     };
 
-    SegmentTree::SegmentTree(int nn) {
-        n = 1;
-        while (n < nn) n *= 2;
-        dat.resize(2 * n - 1, inf);
-    }
-
-    void SegmentTree::update(ll k, ll a) {
+    void RMaxQ::update(ll k, ll a) {
         k += n - 1;
         dat[k] = a;
         while (k > 0) {
             k = (k - 1) / 2;
-            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+            dat[k] = max(dat[k * 2 + 1], dat[k * 2 + 2]);
         }
     }
 
-    ll SegmentTree::query(ll a, ll b, ll k, ll l, ll r) {
-        if (r <= a || b <= l) return inf;
+    ll RMaxQ::query(ll a, ll b, ll k, ll l, ll r) {
+        if (r <= a || b <= l) return -inf;
         if (a <= l && r <= b) return dat[k];
         else {
             ll vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
             ll vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
-            return min(vl, vr);
+            return max(vl, vr);
         }
     }
 
-    ll SegmentTree::getquery(ll a, ll b) {
+    ll RMaxQ::getquery(ll a, ll b) {
         return query(a, b, 0, 0, n - 1);
     }
 
-    ll SegmentTree::operator[](int i) {
+    ll RMaxQ::operator[](int i) {
         return dat[i + n - 1];
     }
+
+    // ------------------------------------------------------
+
+	class RMinQ {
+	public:
+		ll n;
+		vector<ll> dat;
+
+		explicit RMinQ(int nn) {
+			n = 1;
+			while (n < nn) n *= 2;
+			dat.resize(2 * n - 1, inf);
+		}
+
+		void update(ll k, ll a);
+		ll query(ll a, ll b, ll k, ll l, ll r);
+		ll getquery(ll a, ll b);
+		ll operator[](int i);
+	};
+
+	void RMinQ::update(ll k, ll a) {
+		k += n - 1;
+		dat[k] = a;
+		while (k > 0) {
+			k = (k - 1) / 2;
+			dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+		}
+	}
+
+	ll RMinQ::query(ll a, ll b, ll k, ll l, ll r) {
+		if (r <= a || b <= l) return inf;
+		if (a <= l && r <= b) return dat[k];
+		else {
+			ll vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+			ll vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+			return min(vl, vr);
+		}
+	}
+
+	ll RMinQ::getquery(ll a, ll b) {
+		return query(a, b, 0, 0, n - 1);
+	}
+
+	ll RMinQ::operator[](int i) {
+		return dat[i + n - 1];
+	}
 
 }
 
