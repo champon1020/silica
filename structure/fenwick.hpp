3de25d83c1a289a86bc09aa6913b03cd3e537768
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-04-29 07:16:09 +0900

    fix fenwick

--- structure/fenwick.hpp
+++ structure/fenwick.hpp
@@ -5,22 +5,21 @@
 using namespace std;
 
 namespace fenwick {
-
     using ll = long long;
 
     class Fenwick {
     public:
-        ll n;
+        int n;
         vector<ll> bit;
 
-		Fenwick(ll nn, ll e);
-        ll sum(ll i);
-        void add(ll i, ll x);
+		Fenwick(int nn, ll init);
+        ll sum(int i);
+        void add(int i, ll x);
     };
 
-    Fenwick::Fenwick(ll nn, ll e):n(nn), bit(nn + 1, e) {}
+    Fenwick::Fenwick(int n, ll init=0):n(n), bit(n + 1, init) {}
 
-    ll Fenwick::sum(ll i) {
+    ll Fenwick::sum(int i) {
         ll s = 0LL;
         while (i > 0) {
             s += bit[i];
@@ -29,13 +28,12 @@ namespace fenwick {
         return s;
     }
 
-    void Fenwick::add(ll i, ll x) {
+    void Fenwick::add(int i, ll x) {
         while (i <= n) {
             bit[i] += x;
             i += i & -i;
         }
     }
-
 }
 
 #endif //DEFINE_BIT_HPP
