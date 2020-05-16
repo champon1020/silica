3af6c53659cef673b1e101cfc6eb846579840665
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-05-16 19:25:49 +0900

    update debug.hpp

--- debug/debug.hpp
+++ debug/debug.hpp
@@ -11,116 +11,126 @@ namespace dbg {
     using ll = long long;
 
     class Debug {
-        void prefix(string s){
-            cout << "\033[7m\033[35m" << " DEBUG " << "\033[0m\033[35m"
-            << " " << s << "\033[0m" << endl;
-        }
-    public:
-        Debug(){}
-        // vector d1
+        void prefix() { cout << "\033[7m\033[35m" << " DEBUG " << "\033[0m" << endl; }
+
+        void out_helper(){ cout << endl; }
+
+        void out_with_name_helper(){ cout << endl; }
+
+        template<typename T1, typename... T2>
+        void out_helper(T1 v, T2... args);
+
+        template<typename T1, typename... T2>
+        void out_with_name_helper(string s, T1 v, T2... args);
+
+        // value
+        template<typename T>
+        void print(T val);
+
+        // pair
+        template<typename A, typename B>
+        void print(pair<A, B> p);
+
+        // tuple
+        template<typename A, typename B, typename C>
+        void print(tuple<A, B, C> t);
+
+        // vector
         template<typename T>
         void print(vector<T> &vec);
 
-        // vector tuple
-        template<typename T1, typename T2>
-        void print(vector<pair<T1, T2>>& vec, int idx = 0);
-
-        // vector d2
-        template<typename T>
-        void print(vector<vector<T>>& vec);
-
-        // vector d2
-        template<typename T>
-        void print(vector<vector<vector<T>>>& vec);
-
-        // vector array d1
-        template<typename T>
-        void print(T *arr, ll n);
-
-        // vector array d2
-        template<typename T>
-        void print(T *arr, int n, int m);
-
         // set
         template<typename T>
-        void print(set<T>& st);
+        void print(set<T> &st);
 
-        // vector map or set
+        // map
+        template<typename A, typename B>
+        void print(map<A, B> mp);
+    public:
+        Debug(){}
+
+        template<typename... T>
+        void out(T... args){
+            prefix();
+            out_helper(args...);
+        }
+
+        template<typename... T>
+        void out_with_name(T... args){
+            prefix();
+            out_with_name_helper(args...);
+        }
+
         template<typename T>
-        void print(T cont);
+        void outg(vector<vector<T>> &g) {
+            prefix();
+            for(auto& v1 : g){
+                for(auto& v2 : v1) {
+                    cout << v2 << " ";
+                }
+                cout << endl;
+            }
+        }
     };
+
+    template<typename T1, typename... T2>
+    void Debug::out_helper(T1 v, T2... args) {
+        print(v); cout << endl;
+        out_helper(args...);
+    }
+
+    template<typename T1, typename... T2>
+    void Debug::out_with_name_helper(string s, T1 v, T2... args) {
+        cout << s << ": "; print(v); cout << endl;
+        out_with_name_helper(args...);
+    }
+
+    template<typename T>
+    void Debug::print(T val){
+        cout << val;
+    }
+
+    template<typename A, typename B>
+    void Debug::print(pair<A, B> p){
+        cout << "("; print(p.first); cout << ", "; print(p.second); cout << ")";
+    }
+
+    template<typename A, typename B, typename C>
+    void Debug::print(tuple<A, B, C> t) {
+        cout << "("; print(get<0>(t)); cout << ", "; print(get<1>(t)); cout << ", "; print(get<2>(t)); cout << ")";
+    }
 
     template<typename T>
     void Debug::print(vector<T>& vec) {
-        prefix("vector");
-        for (auto& v : vec) cout << v << " ";
-        cout << endl;
-    }
-
-    template<typename T1, typename T2>
-    void Debug::print(vector<pair<T1, T2>>& vec, int idx) {
-        prefix("vector pair");
-        for (auto& v : vec) cout << (!idx ? get<0>(v) : get<1>(v)) << " ";
-        cout << endl;
-    }
-
-    template<typename T>
-    void Debug::print(vector<vector<T>>& vec) {
-        prefix("vector d2");
-        for (auto& vv : vec) {
-            for (auto& v : vv) cout << v << " ";
-            cout << endl;
+        cout << "[";
+        bool fir = true;
+        for(auto& e : vec) {
+            if(!fir) cout << " ";
+            print(e), fir=false;
         }
+        cout << "]";
     }
 
     template<typename T>
-    void Debug::print(vector<vector<vector<T>>>& vec) {
-        prefix("vector d3");
-        for(auto& vvv : vec) {
-            cout << "[";
-            for(int i=0; i<vvv.size(); i++) {
-                if(i) cout << " ";
-                for(int j=0; j<vvv[i].size(); j++) {
-                    if(j) cout << " ";
-                    cout << vvv[i][j];
-                }
-                if(i != vvv.size()-1) cout << endl;
-            }
-            cout << "]" << endl;
+    void Debug::print(set<T> &st){
+        cout << "{";
+        bool fir = true;
+        for(auto& e : st) {
+            if(!fir) cout << " ";
+            print(e), fir=false;
         }
+        cout << "}";
     }
 
-    template<typename T>
-    void Debug::print(T *arr, ll n) {
-        prefix("array");
-        for (int i = 0; i < n; i++) cout << arr[i] << " ";
-        cout << endl;
-    }
-
-    template<typename T>
-    void Debug::print(T *arr, int n, int m) {
-        prefix("array d2");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cout << arr[i][j] << " ";
-            }
-            cout << endl;
+    template<typename A, typename B>
+    void Debug::print(map<A, B> mp){
+        cout << "{";
+        bool fir = true;
+        for(auto& p : mp){
+            if(!fir) cout << " ";
+            print(p), fir=false;
         }
-    }
-
-    template<typename T>
-    void Debug::print(set<T>& st) {
-        prefix("set");
-        for(auto& e : st) cout << e << " ";
-        cout << endl;
-    }
-
-    template<typename T>
-    void Debug::print(T cont) {
-        prefix("key value");
-        for (auto& e : cont)
-            cout << e.first << " : " << e.second << endl;
-        cout << endl;
+        cout << "}";
     }
 
 }
